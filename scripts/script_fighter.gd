81fa9c13adec3b66c54f191ab2e9edac9eb9bb19
Domi <Dominik.janser@mail.com>
2025-08-01 23:30:27 +0200

    layout for functions, added states

--- scripts/script_fighter.gd
+++ scripts/script_fighter.gd
@@ -8,6 +8,8 @@ extends CharacterBody3D
 
 
 @onready var base: RayCast3D = $CollisionShape3D/base
+@onready var label_state: Label = $label_state
+
 
 var state : int = 0
 """
@@ -18,6 +20,8 @@ var state : int = 0
 4.. crouched
 5.. attacking
 6.. being hit
+7.. block
+8.. parry
 """
 var on_floor : bool = false
 
@@ -25,6 +29,8 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	
+	label_state.text = str(state)
 	
 	#on floor
 	on_floor = base.is_colliding()
@@ -36,7 +42,7 @@ func _physics_process(delta):
 
 	#state machine
 	match state:
-		0:
+		0: # ..standing
 			#input
 			if Input.is_action_pressed("key_left"):
 				velocity.x = - run_speed * delta * 100
@@ -47,22 +53,48 @@ func _physics_process(delta):
 			if Input.is_action_pressed("key_jump") && on_floor:
 				velocity.y = jump_speed * delta * 100
 				state = 1
-		1:
+			if Input.is_action_pressed("key_duck"):
+				state = 4
+		1: # ..jumping upward
 			if velocity.y <= 0:
 				state = 2
-		2:
+		2: # ..falling
 			if on_floor:
 				state = 0
-		3:
+		3: # ..lying on the floor
 			pass
-		4:
+		4: # ..crouched
+			if Input.is_action_just_released("key_duck"):
+				state = 0
+		5: # ..attacking
+			#depending what state the player was before what attack is being played
 			pass
-		5:
+		6: # .. being hit
+			#determine how badly the fighter was hit, with stance
 			pass
-		6:
+		7: # ..blocking
+			pass
+		8: #..parrying
 			pass
 		_:
 			pass
 	
 	move_and_slide()
 	
+func _attack_normal(delta):
+	pass
+
+func _attack_jump(delta):
+	pass
+
+func _attack_crouch(delta):
+	pass
+
+func _attack_falling(delta):
+	pass
+
+func _attack_lying(delta):
+	pass
+	
+func _block(delta):
+	pass
