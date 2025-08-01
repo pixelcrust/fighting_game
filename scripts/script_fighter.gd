84c94d381ace943c3f9d7e83f548c2fd2b935dc1
Domi <Dominik.janser@mail.com>
2025-08-01 09:37:25 +0200

    added 2 keys and new states

--- scripts/script_fighter.gd
+++ scripts/script_fighter.gd
@@ -1,8 +1,12 @@
 extends CharacterBody3D
 
 
+@export var hp : float = 100
+@export var stance : float = 100
 @export var run_speed : float = 3.0
 @export var jump_speed : float = 4.0
+
+
 @onready var base: RayCast3D = $CollisionShape3D/base
 
 var state : int = 0
@@ -12,6 +16,8 @@ var state : int = 0
 2.. falling
 3.. lying on floor
 4.. crouched
+5.. attacking
+6.. being hit
 """
 var on_floor : bool = false
 
@@ -50,6 +56,10 @@ func _physics_process(delta):
 		3:
 			pass
 		4:
+			pass
+		5:
+			pass
+		6:
 			pass
 		_:
 			pass
