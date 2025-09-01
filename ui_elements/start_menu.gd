38442d90a0444bd38c6326dfbc8b12e899ae3ad2
Caitlin Cooke <85729182+SapphireTactics@users.noreply.github.com>
2025-09-01 00:27:38 -0700

    Did some UI stuff
    Played around with themes and got the start of the opening animation going.

--- ui_elements/start_menu.gd
+++ ui_elements/start_menu.gd
@@ -1,11 +1,31 @@
-extends Control
+extends Node3D
 
+@onready var open_cam = $Open_Cam2/Open_Cam/Skeleton3D/Camera/Camera
+@onready var mode = $Menu_Mode/Mode
+@onready var open_anim = $Open_Cam2/AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
+var mode_set_local = false
+var mode_set_online = false
+
 func _ready() -> void:
-	pass # Replace with function body.
+	mode.visible = true
+	open_cam.current = true
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if mode_set_local == true:
+		mode.visible = false
+		open_anim.play("Select_Num")
+	elif mode_set_local == false:
+			open_anim.play("Select_Mode")
+		
+	if mode_set_online == true:
+		mode.visible = false
+		open_anim.play("Select_Connection")
+	elif mode_set_online == false:
+			open_anim.play("Select_Mode")
+
+
+
+func _on_local_btn_pressed() -> void:
+	mode_set_local = true
