4066a18afa4385b98fd43142ab834bc4fb2b2c54
Caitlin Cooke <85729182+SapphireTactics@users.noreply.github.com>
2025-09-14 21:40:42 -0700

    New Animations and Rig
    New Rig number 4. It has the block animation and updated set of animations.

---
+++ scripts/character_rig2.gd
@@ -0,0 +1,22 @@
+extends Node3D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+@onready var skeleton_3d: Skeleton3D = $rig_base/Skeleton3D
+
+signal being_hit_to_main_script(dmg : float,stagger : float)
+
+func _process(delta):
+	pass
+
+func _on_physical_bone_being_hit(dmg : float, stagger : float) -> void:
+	print_debug("bone entered collision shape!!!!!!!!!!!!!")
+	print("hallo")
+	emit_signal("being_hit_to_main_script",dmg, stagger)
+	pass # Replace with function body.
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	
+	if is_ancestor_of(body) != true:
+		body.get_hit(10,10)
+		print_debug("body in hitbox")
