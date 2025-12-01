a88568dfed1e3485442ade89e535da8976e35db4
Caitlin Cooke <85729182+SapphireTactics@users.noreply.github.com>
2025-11-30 22:38:39 -0800

    Got the Menu fixed!

--- ui_elements/life_bar.gd
+++ ui_elements/life_bar.gd
@@ -2,9 +2,10 @@ extends Control
 
 var player_id = null
 
-@onready var label_hp: Label = $label_hp
-@onready var label_state: Label = $label_state
+@onready var label_hp: Label = $PanelContainer/Panel/label_hp
+@onready var label_state: Label = $PanelContainer/Panel/label_state
 
 func _process(delta):
-	label_hp.text = str(player_id.hp)
-	label_state.text = str(player_id.state)
+	#label_hp.text = str(player_id.hp)+"%"
+	#label_state.text = str(player_id.state)
+	pass
--- ui_elements/start_menu.gd
+++ ui_elements/start_menu.gd
@@ -1,13 +1,11 @@
 extends Node3D
 
-@onready var open_cam = $Open_Cam2/Open_Cam/Skeleton3D/Camera/Camera
 @onready var mode = $Menu_Mode/Mode
-@onready var open_anim = $Open_Cam2/AnimationPlayer
-@onready var door_anim = $Open_Theater2/AnimationPlayer
-@onready var num_player = $Menu_Mode/Number_Players
+@onready var num = $Menu_Mode/Number_Players
 @onready var arena = $Menu_Mode/Arenas
-@onready var characters = $Menu_Mode/Characters
-@onready var connect = $Menu_Mode/Online_Connect
+@onready var roster = $Menu_Mode/Characters
+@onready var online = $Menu_Mode/Online_Connect
+@onready var videoplayer = $Menu_Clips
 
 var mode_set_local = false
 var mode_set_online = false
@@ -21,98 +19,116 @@ var arena_set: String
 
 func _ready() -> void:
 	mode_set_ready = true
-	open_cam.current = true
-	
-	mode.visible = true
-	num_player.visible = false
+	$Menu_Mode/Mode/HBoxContainer/Local_btn.grab_focus()
+	num.visible = false
 	arena.visible = false
-	connect.visible = false
-	characters.visible = false
-
+	roster.visible = false
+	online.visible = false
+	videoplayer.stream = load("res://ui_elements/Clips/Menu_Mode.ogv")
+	videoplayer.play()
+	
 func _process(_delta: float) -> void:
 	if mode_set_ready == true:
-		open_anim.play("Select_Mode")
+		mode.visible = true
+		
 	
 	if mode_set_local == true:
 		mode.visible = false
-		open_anim.play("Select_Num")
+		
 	
 	if mode_set_online == true:
 		mode.visible = false
-		open_anim.play("Select_Connection")
-		door_anim.play("D02_Open")
+		
 	
 	if num_set_stored == true:
-		num_player.visible = false
-		open_anim.play("Select_Arena")
+		num.visible = false
+		
 		
 	if arena_ready == true:
 		arena.visible = false
-		open_anim.play("Select_Character")
-		door_anim.play("D01_Open")
+		
 		
 	if connect_ready == true:
-		connect.visible = false
-		open_anim.play("Select_Online_Character")
+		online.visible = false
+		
+	
 #Buttons
 func _on_local_btn_pressed() -> void:
 	mode_set_ready = false
 	mode_set_local = true
-
+	videoplayer.stream = load("res://ui_elements/Clips/Menu_Num.ogv")
+	videoplayer.play()
+	videoplayer.loop = false
+	await get_tree().create_timer(2.7).timeout
+	num.visible = true
+	
 func _on_online_btn_pressed() -> void:
 	mode_set_ready = false
 	mode_set_online = true
+	videoplayer.stream = load("res://ui_elements/Clips/Menu_Select Character2.ogv")
+	videoplayer.play()
+	videoplayer.loop = false
+	await get_tree().create_timer(2.7).timeout
+	online.visible = true
 
 func _on_one_player_pressed() -> void:
 	player_count = 1
 	num_set_stored = true
+	videoplayer.stream = load("res://ui_elements/Clips/Menu_Arena.ogv")
+	videoplayer.play()
+	videoplayer.loop = false
+	await get_tree().create_timer(1).timeout
+	arena.visible = true
 	
 func _on_two_player_pressed() -> void:
 	player_count = 2
 	num_set_stored = true
+	videoplayer.stream = load("res://ui_elements/Clips/Menu_Arena.ogv")
+	videoplayer.play()
+	videoplayer.loop = false
+	await get_tree().create_timer(1).timeout
+	arena.visible = true
 	
 func _on_three_player_pressed() -> void:
 	player_count = 3
 	num_set_stored = true
+	videoplayer.stream = load("res://ui_elements/Clips/Menu_Arena.ogv")
+	videoplayer.play()
+	videoplayer.loop = false
+	await get_tree().create_timer(1).timeout
+	arena.visible = true
 	
 func _on_four_player_pressed() -> void:
 	player_count = 4
 	num_set_stored = true
+	videoplayer.stream = load("res://ui_elements/Clips/Menu_Arena.ogv")
+	videoplayer.play()
+	videoplayer.loop = false
+	await get_tree().create_timer(1).timeout
+	arena.visible = true
 
 func _on_arena_01_pressed() -> void:
 	arena_set = "arena_01"
 	arena_ready = true
+	videoplayer.stream = load("res://ui_elements/Clips/Menu_Select Character.ogv")
+	videoplayer.play()
+	videoplayer.loop = false
+	await get_tree().create_timer(1.5).timeout
+	roster.visible = true
 
 func _on_pass_pressed() -> void:
 	connect_ready = true
+	videoplayer.stream = load("res://ui_elements/Clips/Menu_Fight.ogv")
+	videoplayer.play()
+	videoplayer.loop = false
+	await get_tree().create_timer(3).timeout
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
 	
-
-#Animation Transitions
-func _on_animation_player_animation_finished(anim_name) -> void:
-	if anim_name == "Select_Num":
-		mode_set_local = false
-		num_player.visible = true
-	
-	if anim_name == "Select_Connection":
-		mode_set_online = false
-		connect.visible = true
-		
-	if anim_name == "Select_Arena":
-		num_set_stored = false
-		arena.visible = true
-		
-	if anim_name == "Select_Character":
-		arena_ready = false
-		characters.visible = true
-		
-	if anim_name == "Select_Online_Character":
-		connect_ready = false
-		characters.visible = true
-
-
-func _on_animation_player_door_finished(anim_name: StringName) -> void:
-	pass # Replace with function body.
-
 
 func _on_usher_pressed() -> void:
+	roster.visible = false
+	videoplayer.stream = load("res://ui_elements/Clips/Menu_Fight.ogv")
+	videoplayer.play()
+	videoplayer.loop = false
+	await get_tree().create_timer(3).timeout
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
